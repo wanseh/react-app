4f62a3419383fc2a1164004533cddb3857e881de
wanseh <lanceparantar11@gmail.com>
2025-03-25 10:47:41 +0800

    Add Appwrite integration for trending movies and search count tracking

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Search from "./components/Search";
 import Spinner from "./components/Spinner";
 import MovieCard from "./components/MovieCard";
 import { useDebounce } from "react-use";
+import { getTrendingMovies, updateSearchCount } from "./appwrite";
 
 const API_BASE_URL = "https://api.themoviedb.org/3";
 
@@ -17,11 +18,12 @@ const API_OPTIONS = {
 };
 
 const App = () => {
+  const [isLoading, setIsLoading] = useState(false);
   const [searchTerm, setSearchTerm] = useState("");
   const [errorMessage, setErrorMessage] = useState("");
   const [movies, setMovies] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
   const [debounceSearchTerm, setDebounceSearchTerm] = useState("");
+  const [trendingMovies, setTrendingMovies] = useState([]);
 
   const fetchMovies = async (query = "") => {
     setIsLoading(true);
@@ -45,6 +47,10 @@ const App = () => {
       }
 
       setMovies(data.results || []);
+
+      if (query && data.results.length > 0) {
+        await updateSearchCount(query, data.results[0]);
+      }
     } catch (error) {
       console.error("Error fetching movies:", error);
       setErrorMessage(
@@ -64,10 +70,23 @@ const App = () => {
     500,
     [searchTerm]
   );
+  const fetchTrendingMovies = async () => {
+    try {
+      const trending = await getTrendingMovies();
+      setTrendingMovies(trending);
+    } catch (error) {
+      console.error("Error fetching trending movies:", error);
+    }
+  };
  
   useEffect(() => {
     fetchMovies(debounceSearchTerm);
   }, [debounceSearchTerm]);
+
+  useEffect(() => {
+    fetchTrendingMovies();
+  }, [])
+  
 
   return (
     <main>
@@ -77,12 +96,28 @@ const App = () => {
             <img src="./hero.png" alt="" />
             <h1>
               Find <span className="text-gradient">Movies</span> You'll Enjoy
-              Without the Hassle{" "}
+              Without the Hassle{" "};
             </h1>
             <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
           </header>
           <section className="all-movies">
-            <h2 className="mt-[40px]">All Movies</h2>
+            {trendingMovies.length > 0 && (
+              <section className="trending">
+                <h2>Trending Movies</h2>
+
+                <ul>
+                  {trendingMovies.map((movie, index) => (
+                    <li key={movie.$id}>
+                      <p>{index + 1}</p>
+                      <img src={movie.poster_url} alt={movie.title} />
+                    </li>
+                  ))}
+                </ul>
+              </section>
+            )}
+
+            <h2>All Movies</h2>
+
             {isLoading ? (
               <Spinner />
             ) : errorMessage ? (
